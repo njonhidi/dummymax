faa155fece3f4dfdb482176a89badc42ec3d8fc1
njonhidi <njonhidi@gmail.com>
2026-09-22 12:14:26 +0300

    Update ohtr.sh

--- ohtr.sh
+++ ohtr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-		echo '<?xml version="1.0" encoding="UTF-8"?>' >> ohtrtsl.txt
+		echo '<?xml version="1.0" encoding="UTF-8"?>' >> https://github.com/njonhidi/dummymax/ohtrtsl.txt
 		
 		
