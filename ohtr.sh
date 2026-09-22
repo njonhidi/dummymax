c2a83eeffc87b1086fecbdd3dc200d06d54ac967
njonhidi <njonhidi@gmail.com>
2026-09-22 13:42:56 +0300

    Update ohtr.sh

--- ohtr.sh
+++ ohtr.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-		echo '<?xml version="1.0" encoding="UTF-8"?>' >> ohtr.txt
-git	add ohtr.txt	
-git commit -m
-git push origin main
+		echo '<?xml version="1.0" encoding="UTF-8"?>' >> /github/Dummymax/ohtr.txt
+
 		
